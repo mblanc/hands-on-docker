3f606fe8b3d8f0e8e034b757bc901e77d5c4f478
root <root@ip-10-72-135-203.eu-west-1.compute.internal>
2013-11-30 22:33:29 +0000

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 docker run -d -name mongo mblanc/mongo --noprealloc --smallfiles
-docker run -d -link mongo:mongo mblanc/express-todo-example
+docker run -d -link mongo:mongo -p 80:8080 mblanc/express-todo-example
